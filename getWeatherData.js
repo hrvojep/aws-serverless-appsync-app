970fe02049657f6446d6f9185dabcb92908dba41
hrvojep <hrvojep@yahoo.com>
2018-08-25 19:32:09 +1000

    Update getWeatherData.js

--- getWeatherData.js
+++ getWeatherData.js
@@ -1,16 +1,18 @@
 var http =  require('http');
 const querystring = require('querystring');
+const util = require('util');
 
 exports.handler = (event, context, callback) => {
     
-    console.log(event);
-    
+   // console.log(event);
+    console.log("event: ", util.inspect(event, { showHidden: false, depth: null }));
+
     var city = querystring.escape(event.city);
     
     http.get({
       hostname: 'api.openweathermap.org',
       port: 80,
-      path: '/data/2.5/forecast?q='+ city +'&appid=' + process.env.APPID + '&cnt=3&units=imperial',
+      path: '/data/2.5/forecast?q='+ city +'&appid=' + process.env.APPID + '&cnt=3&units=metric',
       agent: false  // create a new agent just for this one request
     }, (res) => {
        
